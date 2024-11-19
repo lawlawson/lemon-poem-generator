9bbca8157fcfa92a6140bf8212f34222303f881e
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-11-19 18:54:40 +0000

    add openai endpoint & test endpoint

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import chatCompletion from './services/openai.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello from my lovely new server!');
+});
+
+app.get('/test', async (req, res) => {
+  const response = await chatCompletion('Provide an amusing sentence.');
+  res.send(response);
 });
 
 export default app;
