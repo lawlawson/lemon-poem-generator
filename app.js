66d3d0e18e8939bc29336c97022f011f0f7773a0
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-11-19 21:55:24 +0000

    add public HTML & get-poem endpoint

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ import chatCompletion from './services/openai.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from my lovely new server!');
-});
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from my lovely new server!');
+// });
 
 app.get('/test', async (req, res) => {
   const response = await chatCompletion('Provide an amusing sentence.');
   res.send(response);
+});
+
+app.get('/get-poem', async (req, res) => {
+  const response = await chatCompletion(
+    'Write a short, expressive poem about lemon cake, as if you are hungry. Just four lines!'
+  );
+  res.send({ poem: response });
 });
 
 export default app;
